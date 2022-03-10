97d5777d1f2880e95059e7becfacbfed6c4e7bb7
jwerle <joseph.werle@gmail.com>
2022-03-10 17:55:50 -0500

    chore(): add missing type signature

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import "github.com/aws/aws-lambda-go/lambda"
 import "github.com/aws/aws-lambda-go/events"
 
-func handleRequest () (events.APIGatewayProxyResponse, error) {
+import "encoding/json"
+import "context"
+
+func handleRequest (ctx context.Context, event json.RawMessage) (events.APIGatewayProxyResponse, error) {
     return events.APIGatewayProxyResponse{
         StatusCode: 200,
         Body: "\"Hello from Go!\"",
